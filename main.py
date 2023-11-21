e646971640123e996a6b83f190bac5cb6e17ebe7
PedC18 <PedCMF18@gmail.com>
2023-11-20 22:16:24 -0300

    main.py

--- main.py
+++ main.py
@@ -1,92 +1,106 @@
+from lightfm import LightFM
+from lightfm.evaluation import auc_score
 import pandas as pd
-import sys
-from typing import Dict, Text
-
 import numpy as np
-import tensorflow as tf
-
-import tensorflow_datasets as tfds
-import tensorflow_recommenders as tfrs
-
-class MovieLensModel(tfrs.Model):
-  # We derive from a custom base class to help reduce boilerplate. Under the hood,
-  # these are still plain Keras Models.
-
-  def __init__(
-      self,
-      user_model: tf.keras.Model,
-      movie_model: tf.keras.Model,
-      task: tfrs.tasks.Retrieval):
-    super().__init__()
-
-    # Set up user and movie representations.
-    self.user_model = user_model
-    self.movie_model = movie_model
-
-    # Set up a retrieval task.
-    self.task = task
-
-  def compute_loss(self, features: Dict[Text, tf.Tensor], training=False) -> tf.Tensor:
-    # Define how the loss is computed.
-
-    user_embeddings = self.user_model(features["UserId"])
-    movie_embeddings = self.movie_model(features["ItemId"])
-
-    return self.task(user_embeddings, movie_embeddings)
-
+from lightfm.data import Dataset
+from lightfm_dataset_helper.lightfm_dataset_helper import DatasetHelper
+import re
+import sys
 
 Ratings = pd.read_json(sys.argv[1], lines=True)
+ratings_id = Ratings['UserId'].unique()
 Content = pd.read_json(sys.argv[2], lines=True)
+
 Target = pd.read_csv(sys.argv[3])
 
-ratings_util = Ratings[["UserId", "ItemId", "Rating"]]
-Ratings = tf.data.Dataset.from_tensor_slices(dict(ratings_util))
-content_util = Content[["ItemId", "Title","Year", "Rated", "Released", "Runtime", "Genre", "Director"]]
-Content = tf.data.Dataset.from_tensor_slices(dict(content_util))
+ratings_id = Ratings['UserId'].unique()
+ratings_dict = pd.DataFrame(ratings_id)
+ratings_dict.insert(0, 'id', range(1, len(ratings_dict) + 1))
+ratings_dict = dict(zip(ratings_dict[0], ratings_dict['id']))
 
-ratings = Ratings.map(lambda x: {
-    "ItemId": x["ItemId"],
-    "UserId": x["UserId"]
-})
+content_id = Content['ItemId'].unique()
+content_dict = pd.DataFrame(content_id)
+content_dict.insert(0, 'id', range(1, len(content_dict) + 1))
 
-movies = Content.map(lambda x: x["ItemId"])
+content_dict = dict(zip(content_dict[0], content_dict['id']))
 
+dataset = Dataset()
+dataset.fit((ratings_id),
+    (content_id))
 
-user_ids_vocabulary = tf.keras.layers.StringLookup(mask_token=None)
-user_ids_vocabulary.adapt(ratings.map(lambda x: x["UserId"]))
+num_users, num_items = dataset.interactions_shape()
 
-movie_titles_vocabulary = tf.keras.layers.StringLookup(mask_token=None)
-movie_titles_vocabulary.adapt(movies)
+def extract_first_rating_value(ratings_list):
+    if ratings_list:
+        return ratings_list[0]["Value"]
+    else:
+        return None
+    
+# Aplicar a função à coluna "Ratings" para criar uma nova coluna "FirstRatingValue"
+Content['FirstRatingValue'] = Content['Ratings'].apply(extract_first_rating_value)
 
+def extract_numbers(text):
+    # Use uma expressão regular para encontrar todos os números na string
+    numbers = re.findall(r'\b\d+\b', str(text))
+    # Soma os números encontrados (se houver algum)
+    return sum(map(int, numbers)) if numbers else 0
 
-# Define user and movie models.
-user_model = tf.keras.Sequential([
-    user_ids_vocabulary,
-    tf.keras.layers.Embedding(user_ids_vocabulary.vocab_size(), 64)
-])
-movie_model = tf.keras.Sequential([
-    movie_titles_vocabulary,
-    tf.keras.layers.Embedding(movie_titles_vocabulary.vocab_size(), 64)
-])
+# Aplicar a função à coluna "Awards" para criar uma nova coluna "QtdAwards"
+Content['QtdAwards'] = Content['Awards'].apply(extract_numbers)
 
-# Define your objectives.
-task = tfrs.tasks.Retrieval(metrics=tfrs.metrics.FactorizedTopK(
-    movies.batch(128).map(movie_model)
-  )
-)
+dataset.fit_partial(items=Content['ItemId'],
+                    item_features=(Content['Metascore']))
+dataset.fit_partial(items=Content['ItemId'],
+                    item_features=(Content['imdbRating']))
+dataset.fit_partial(items=Content['ItemId'],
+                    item_features=(Content['Genre']))
+dataset.fit_partial(items=Content['ItemId'],
+                    item_features=(Content['Director']))
+dataset.fit_partial(items=Content['ItemId'],
+                    item_features=(Content['FirstRatingValue']))
+dataset.fit_partial(items=Content['ItemId'],
+                    item_features=(Content['QtdAwards']))
 
-# Create a retrieval model.
-model = MovieLensModel(user_model, movie_model, task)
-model.compile(optimizer=tf.keras.optimizers.Adagrad(0.5))
+num_users, num_items = dataset.interactions_shape()
 
-# Train for 3 epochs.
-model.fit(ratings.batch(4096), epochs=3)
+tuple_interactions = Ratings[['UserId', 'ItemId', 'Rating']]
+tuple_interactions = list(tuple_interactions.to_records(index=False))
 
-# Use brute-force search to set up retrieval using the trained representations.
-index = tfrs.layers.factorized_top_k.BruteForce(model.user_model)
-index.index_from_dataset(
-    movies.batch(100).map(lambda title: (title, model.movie_model(title))))
+(interactions, weights) = dataset.build_interactions(tuple_interactions)
 
-# Get some recommendations.
-_, titles = index(np.array(["c4ca4238a0"]))
-print(f"Top 3 recommendations for user c4ca4238a0: {titles[0, :3]}")
+tuple_content = Content[["ItemId", "Metascore", "imdbRating", "Genre", "Director", "FirstRatingValue", "QtdAwards"]]
+tuple_content = [(row['ItemId'], [row["Metascore"],  row["imdbRating"], row["Genre"], row["Director"], row["FirstRatingValue"], row["QtdAwards"]]) for _, row in tuple_content.iterrows()]
+
+item_feat = dataset.build_item_features(tuple_content)
+
+model = LightFM(loss='warp',
+                learning_rate=0.005,
+                learning_schedule='adagrad',
+                no_components=5)
+
+model.fit(interactions,
+          sample_weight=weights ,
+          item_features=item_feat,
+          epochs=10)
+
+def getItemModelId(userId, Target):
+    data = []
+    usar = Target.loc[Target['UserId'] == userId].copy()
+    usar = usar['ItemId'].copy()
+    for item in usar:
+        itemId = content_dict.get(item) - 1
+        data.append(int(itemId))
+
+    return data
+
+targets = Target['UserId'].unique()
+csv_filename = 'output.csv'
+with open(csv_filename, 'w+') as csv_file:
+    csv_file.write('UserId,ItemId\n')
+    for target in targets:
+        customer_id = ratings_dict.get(target) - 1
+        item_ids_cont = getItemModelId(target, Target)
+        predictions = model.predict(customer_id, item_ids=item_ids_cont,item_features=item_feat, num_threads=4)
+        top_items = sorted(zip(item_ids_cont, predictions), key=lambda x: -x[1])[:100]
+        for rec in top_items:
+            csv_file.write(target +','+content_id[rec[0]]+ '\n')
